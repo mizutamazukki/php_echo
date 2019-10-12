ca830942dd3fa7f85e75679a81f0ca00f4e08454
樋浦 <mizutama12120329@gmail.com>
2019-10-12 22:45:00 +0900

    create frame work

---
+++ htdocs/views/blog.php
@@ -0,0 +1,10 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <h1>aaa<?php echo $str; ?></h1>
+    </body>
+</html>
+© 2019 GitHub, Inc.
